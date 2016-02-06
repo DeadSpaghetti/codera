6f241a6b62b6dfd9e4d9f301b691d298687def96
spaghettic0der <max.wittig95@gmail.com>
2016-02-06 21:19:11 +0100

    saveAsJSONStuff #7 done

--- php/addProjectToJSON.php
+++ php/addProjectToJSON.php
@@ -1,8 +1,35 @@
 <?php
 
+function generateGuid($include_braces = false)
+{
+    if (function_exists('com_create_guid')) {
+        if ($include_braces === true) {
+            return com_create_guid();
+        } else {
+            return substr(com_create_guid(), 1, 36);
+        }
+    } else {
+        mt_srand((double) microtime() * 10000);
+        $charid = strtoupper(md5(uniqid(rand(), true)));
+
+        $guid = substr($charid,  0, 8) . '-' .
+            substr($charid,  8, 4) . '-' .
+            substr($charid, 12, 4) . '-' .
+            substr($charid, 16, 4) . '-' .
+            substr($charid, 20, 12);
+
+        if ($include_braces) {
+            $guid = '{' . $guid . '}';
+        }
+
+        return $guid;
+    }
+}
+
+
     $date = $_POST['date'];
     $name = $_POST['name'];
-    $name = "max";
+    $name = $_POST['name'];
     $icon = $_POST['icon']; //path to icon
     $versionName = $_POST['versionName'];
     $latestChanges = $_POST['latestChanges'];
@@ -13,49 +40,45 @@
     $license = $_POST['license'];
     $versionCode = $_POST['versionCode'];
 
-$pathString = "../config/projects.json";
+    $projectGUID = generateGuid();
+    echo $projectGUID;
+    $pathString = "../config/projects.json";
 
 
-if(file_exists($pathString))
-{
-    $configFile = file_get_contents($pathString);
-    try
-    {
-        $fileInJSON = json_decode($configFile);
-        //var_dump($fileInJSON);
+    if (file_exists($pathString)) {
+        $configFile = file_get_contents($pathString);
+        try {
+            $fileInJSON = json_decode($configFile);
+            //var_dump($fileInJSON);
 
-        //save to php format array
-        $array = array($fileInJSON);
-        $newProjectArray = array("name" => $name,
-                                 "icon" => $icon,
-                                 "versionName" => $versionName,
-                                 "date" => $date,
-                                 "latestChanges" => $latestChanges,
-                                 "files" => $files,
-                                 "screenshots" => $screenshots,
-                                 "license" => $license,
-                                 "versionCode" => $versionCode);
+            //save to php format array
+            $array = array($fileInJSON);
+            $newProjectArray = array("name" => $name,
+                "icon" => $icon,
+                "versionName" => $versionName,
+                "date" => $date,
+                "latestChanges" => $latestChanges,
+                "files" => $files,
+                "screenshots" => $screenshots,
+                "license" => $license,
+                "versionCode" => $versionCode,
+                "GUID" => $projectGUID);
 
-        array_push($array,$newProjectArray);
+            array_push($array, $newProjectArray);
 
-        //push new reg id into array
-        $fileToSave = json_encode($array);
-        echo $fileToSave;
-        $file = fopen($pathString,"w");
-        fwrite($file,$fileToSave);
+            //push new reg id into array
+            $fileToSave = json_encode($array);
+            echo $fileToSave;
+            $file = fopen($pathString, "w");
+            fwrite($file, $fileToSave);
+
+
+        } catch (Exception $e) {
+            var_dump($e);
+        }
 
 
     }
-    catch(Exception $e)
-    {
-        var_dump($e);
-    }
-
-
-
-}
-
-
 
 
 
