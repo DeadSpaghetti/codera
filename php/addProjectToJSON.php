0ea4c9dfae56f3ae23ac1bee3533cb6f2a46695c
spaghettic0der <max.wittig95@gmail.com>
2016-02-06 21:36:24 +0100

    saveAsJSONStuff projects.json is now being generated automatically, if it doesn't already exist

--- php/addProjectToJSON.php
+++ php/addProjectToJSON.php
@@ -2,23 +2,30 @@
 
 function generateGuid($include_braces = false)
 {
-    if (function_exists('com_create_guid')) {
-        if ($include_braces === true) {
+    if (function_exists('com_create_guid'))
+    {
+        if ($include_braces === true)
+        {
             return com_create_guid();
-        } else {
+        }
+        else
+        {
             return substr(com_create_guid(), 1, 36);
         }
-    } else {
-        mt_srand((double) microtime() * 10000);
+    }
+    else
+    {
+        mt_srand((double)microtime() * 10000);
         $charid = strtoupper(md5(uniqid(rand(), true)));
 
-        $guid = substr($charid,  0, 8) . '-' .
-            substr($charid,  8, 4) . '-' .
+        $guid = substr($charid, 0, 8) . '-' .
+            substr($charid, 8, 4) . '-' .
             substr($charid, 12, 4) . '-' .
             substr($charid, 16, 4) . '-' .
             substr($charid, 20, 12);
 
-        if ($include_braces) {
+        if ($include_braces)
+        {
             $guid = '{' . $guid . '}';
         }
 
@@ -27,58 +34,77 @@ function generateGuid($include_braces = false)
 }
 
 
-    $date = $_POST['date'];
-    $name = $_POST['name'];
-    $name = $_POST['name'];
-    $icon = $_POST['icon']; //path to icon
-    $versionName = $_POST['versionName'];
-    $latestChanges = $_POST['latestChanges'];
-    $description = $_POST['description'];
-    $requirements = $_POST['requirements'];
-    $files = $_POST['files'];
-    $screenshots = $_POST['screenshots'];
-    $license = $_POST['license'];
-    $versionCode = $_POST['versionCode'];
+$date = $_POST['date'];
+$name = $_POST['name'];
+$name = $_POST['name'];
+$icon = $_POST['icon']; //path to icon
+$versionName = $_POST['versionName'];
+$latestChanges = $_POST['latestChanges'];
+$description = $_POST['description'];
+$requirements = $_POST['requirements'];
+$files = $_POST['files'];
+$screenshots = $_POST['screenshots'];
+$license = $_POST['license'];
+$versionCode = $_POST['versionCode'];
 
-    $projectGUID = generateGuid();
-    echo $projectGUID;
-    $pathString = "../config/projects.json";
+$projectGUID = generateGuid();
+echo $projectGUID;
+$pathString = "../config/projects.json";
 
+$isFileThere = false;
 
-    if (file_exists($pathString)) {
-        $configFile = file_get_contents($pathString);
-        try {
+if (file_exists($pathString))
+{
+
+    $isFileThere = true;
+}
+
+    try
+    {
+        if($isFileThere)
+        {
+            $configFile = file_get_contents($pathString);
             $fileInJSON = json_decode($configFile);
             //var_dump($fileInJSON);
 
             //save to php format array
             $array = array($fileInJSON);
-            $newProjectArray = array("name" => $name,
-                "icon" => $icon,
-                "versionName" => $versionName,
-                "date" => $date,
-                "latestChanges" => $latestChanges,
-                "files" => $files,
-                "screenshots" => $screenshots,
-                "license" => $license,
-                "versionCode" => $versionCode,
-                "GUID" => $projectGUID);
-
-            array_push($array, $newProjectArray);
-
-            //push new reg id into array
-            $fileToSave = json_encode($array);
-            echo $fileToSave;
-            $file = fopen($pathString, "w");
-            fwrite($file, $fileToSave);
-
-
-        } catch (Exception $e) {
-            var_dump($e);
         }
+        $newProjectArray = array(
+            "name" => $name,
+            "icon" => $icon,
+            "versionName" => $versionName,
+            "date" => $date,
+            "latestChanges" => $latestChanges,
+            "files" => $files,
+            "screenshots" => $screenshots,
+            "license" => $license,
+            "versionCode" => $versionCode,
+            "GUID" => $projectGUID
+        );
+
+        if($isFileThere)
+            array_push($array, $newProjectArray);
+        else
+            $array = $newProjectArray;
+
+        //push new reg id into array
+        $fileToSave = json_encode($array);
+        echo $fileToSave;
+        $file = fopen($pathString, "w");
+        fwrite($file, $fileToSave);
+        fclose($file);
 
 
     }
+    catch (Exception $e)
+    {
+        var_dump($e);
+    }
+
+
+
+
 
 
 
