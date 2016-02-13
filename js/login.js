1d715c1062f26bb3018112b80a50206db5098e1b
spaghettic0der <max.wittig95@gmail.com>
2016-02-13 21:04:20 +0100

    fixed #30

--- js/login.js
+++ js/login.js
@@ -1,27 +1,30 @@
 $(document).ready(function()
 {
-	document.getElementById('login-message') .style.display='none';			
+	document.getElementById('login-message').style.display='none';
 	
     $('#button-login').click(function()
     {
         var username = $('#input-username').val();
         var password = $('#input-password').val();
 
-        $.post('../php/checkLogin.php',
-            {
-                "username": username,
-                "password": password
+        if(username != "" && username != undefined && password != "" && password != undefined)
+        {
+            $.post('../php/checkLogin.php',
+                {
+                    "username": username,
+                    "password": password
 
-            },function(data,error)
-            {     
-				if(data == "success")
-				{
-					location.href = "./restricted/admin.php"
-				}	
-				else
-				{				
-					document.getElementById('login-message') .style.display='';						
-				}
-            });
+                }, function (data, error)
+                {
+                    if (data == "success")
+                    {
+                        location.href = "./restricted/admin.php"
+                    }
+                    else
+                    {
+                        document.getElementById('login-message').style.display = '';
+                    }
+                });
+        }
     });
 });
