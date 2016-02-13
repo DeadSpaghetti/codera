897c7caa96df49ef4389c61a838ccd9cf414b7b4
spaghettic0der <max.wittig95@gmail.com>
2016-02-13 23:03:42 +0100

    changed gitignore

--- js/generalSettings.js
+++ js/generalSettings.js
@@ -2,7 +2,8 @@ $(document).ready(function()
 {
     $('button-save').click(function()
     {
-
+        var developerName = $('#input-websiteName').val();
+        var colorScheme = $('#color-chooser :selected').text();
 
 
     });
