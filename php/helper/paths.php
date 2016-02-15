5e32c10e789519c56022fdb0200bdd0a26691dd7
deadlocker8 <goldi.96@gmx.de>
2016-02-15 21:30:34 +0100

    Fixed #45

--- php/helper/paths.php
+++ php/helper/paths.php
@@ -1,3 +1,4 @@
 <?php
 $path_config_generalSettings = realpath(dirname(__FILE__) . '/../../config/generalSettings.json');
+$path_helper_getGeneralSettings = realpath(dirname(__FILE__) . '/getGeneralSettingsFromJSON.php')
 ?>
--- php/templates/footer.php
+++ php/templates/footer.php
@@ -1,8 +1,12 @@
+<?php
+include(realpath(dirname(__FILE__) . '/../helper/paths.php'));
+include($path_helper_getGeneralSettings);
+?>
 <div id="footer">	
 	<table>
 		<tr>
 			<td> <a href="about.php">About</a></td>
-			<td>&copy; Deadlocker <?php echo date("Y");?></td>
+			<td>&copy; <?php echo $developerName;?> <?php echo date("Y");?></td>
 			<td> <div id="footer-right"><a href="https://github.com/spaghettic0der/codera"> <img id="icon-github" src="../../images/icons/GitHub-icon.png" alt=""> <span id="footer-right-text">codera on GitHub</span></a></div><div style="clear: both;"></div> </td>
 		</tr>
 	</table>		
