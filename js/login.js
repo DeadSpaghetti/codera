ee1a09047af91a3519fde1eb2b623ef8363c5672
deadlocker8 <goldi.96@gmx.de>
2016-02-21 15:19:45 +0100

    Fixed #49

--- js/login.js
+++ js/login.js
@@ -3,22 +3,22 @@ $(document).ready(function()
 	document.getElementById('login-message').style.display='none';
 	
     $('#button-login').click(function()
-    {
+    {	
         var username = $('#input-username').val();
         var password = $('#input-password').val();
 
         if(username != "" && username != undefined && password != "" && password != undefined)
-        {
+        {		
             $.post('../php/helper/checkLogin.php',
                 {
                     "username": username,
                     "password": password
 
                 }, function (data, error)
-                {
-                    if (data == "success")
-                    {						
-                        location.href = "../php/restricted/admin.php"
+                {					
+                    if (data.indexOf("success") > -1)
+                    {							
+                        location.href = "../php/restricted/admin.php";
                     }
                     else
                     {
