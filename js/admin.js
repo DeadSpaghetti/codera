b2996ac53e83417777c633f029322e6e04220ab4
spaghettic0der <max.wittig95@gmail.com>
2016-02-22 21:08:44 +0100

    I give up on the tab thing for now

--- js/admin.js
+++ js/admin.js
@@ -1,25 +1,5 @@
 $(document).ready(function ()
 {
-    openTab();
-
-    function openTab()
-    {
-        /*
-        if(localStorage.getItem("activeTab") != null)
-        {
-            var activeTabID = localStorage.getItem("activeTab");
-            var activeTab = $('.tabs '+ activeTabID);
-            activeTab.show();
-            activeTab.siblings().hide();
-
-
-            $(activeTabID).parent().addClass("active");
-            console.log($(activeTabID).parent());
-            $('.tabs .tab-links a ' +activeTabID)
-        }
-        */
-    }
-
     //tabView
     $('.tabs .tab-links a').on('click', function(e)
     {
@@ -28,8 +8,6 @@ $(document).ready(function ()
         $('.tabs ' + currentAttrValue).show().siblings().hide();
         // Change/remove current tab to active
         $(this).parent('li').addClass('active').siblings().removeClass('active');
-
-        localStorage.setItem("activeTab",$(this).attr('href'));
         e.preventDefault();
 
     });	
