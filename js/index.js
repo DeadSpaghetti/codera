64bacb0d36fb76ca862e740834ef90cd1d3528b0
deadlocker8 <goldi.96@gmx.de>
2016-03-12 12:42:17 +0100

    Fixed #47
    alpha and beta banners now dynamically on index.php

--- js/index.js
+++ js/index.js
@@ -10,10 +10,61 @@ $(document).ready(function()
         input.name = "UUID";
         input.value = this.id;
 
-
         form.appendChild(input);
         document.body.appendChild(form);
         form.submit();
     });
 
 });
+
+function makeRequestAndSetBanners(currentCanvas,color,id)
+{	
+    $.post("helper/getProjectStatus.php",
+        {
+            "UUID": id.toString().trim()
+        },function(data,error)
+        {
+            var status = data;
+				
+            if(currentCanvas.getContext != null)
+            {
+                var ctx = currentCanvas.getContext("2d");
+
+                var width = currentCanvas.width;
+                var height = currentCanvas.height;
+
+                ctx.font = "55px Roboto";
+
+                if(status == "Alpha")
+                {
+                    ctx.fillStyle = color;
+                    ctx.fillRect(0.3*width, 0.75*height, 0.7*width, 0.25*height);
+                    ctx.fillStyle = "#FFFFFF";
+                    ctx.fillText(status.toString().toUpperCase(),0.36*width, 0.94*height);
+                }
+                else if(status == "Beta")
+                {
+                    ctx.fillStyle = color;
+                    ctx.fillRect(0.3*width, 0.75*height, 0.7*width, 0.25*height);
+                    ctx.fillStyle = "#FFFFFF";
+                    ctx.fillText(status.toString().toUpperCase(),0.46*width, 0.94*height);
+                }
+            }
+        }
+    );
+}
+
+function drawBanners(color)
+{
+
+	var links = document.getElementsByName("icon-link");
+	
+	for(var i = 0; i < links.length; i++)
+	{		
+		var currentLink = links[i];
+		var id = currentLink.id;
+		var currentCanvas = currentLink.childNodes[0].childNodes[1];		
+	
+        makeRequestAndSetBanners(currentCanvas,color,id);
+	}
+}
--- php/index.php
+++ php/index.php
@@ -17,9 +17,9 @@ include($path_helper_getGeneralSettings);
 		<link type="text/css" rel="stylesheet" href="../css/stylesheet-main.css"/>
 		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-		<script src="../js/index.js"></script>
+		<script src="../js/index.js"></script>		
 	</head>
-	<body>
+	<body onload="drawBanners('<?php echo $colorScheme;?>');">
 		<div id="main">
 			<?php 				
 				if(isset($_SESSION['loggedIn']))
@@ -54,11 +54,14 @@ include($path_helper_getGeneralSettings);
 								
 								if($gridSize == 3)
 								{
-									echo	'<td class="entry large">'.
-												'<a id="'.$UUID.'">'.
-													'<div class="entry-icon large" style="background-image: url('."'".$path_folder_icons.'/'.$icon."'".');"> </div>'.
+									echo	'<td class="entry large">'.												
+												'<a name="icon-link" id="'.$UUID.'">'.
+													'<div class="entry-icon large container">'.
+														'<div class="entry-icon" style="background-image: url('."'".$path_folder_icons.'/'.$icon."'".');"> </div>'.
+														'<canvas width="300" height="300"></canvas>'.														
+													'</div>'.
 													'<div class="icon-name">'.$name.'</div>'.
-												'</a>'.
+												'</a>'.												
 											'</td>';
 											
 									if(fmod($i, 3) == 2)
@@ -68,11 +71,14 @@ include($path_helper_getGeneralSettings);
 								}
 								else if($gridSize == 4)
 								{
-									echo	'<td class="entry medium">'.
-												'<a id="'.$UUID.'">'.
-													'<div class="entry-icon medium" style="background-image: url('."'".$path_folder_icons.'/'.$icon."'".');"> </div>'.
+									echo	'<td class="entry medium">'.												
+												'<a name="icon-link" id="'.$UUID.'">'.
+													'<div class="entry-icon medium container">'.
+														'<div class="entry-icon" style="background-image: url('."'".$path_folder_icons.'/'.$icon."'".');"> </div>'.
+														'<canvas width="300" height="300"></canvas>'.														
+													'</div>'.
 													'<div class="icon-name">'.$name.'</div>'.
-												'</a>'.										
+												'</a>'.												
 											'</td>';
 											
 									if(fmod($i, 4) == 3)
@@ -82,11 +88,14 @@ include($path_helper_getGeneralSettings);
 								}
 								else
 								{
-									echo	'<td class="entry small">'.
-												'<a id="'. $UUID.'">'.
-													'<div class="entry-icon small" style="background-image: url('."'".$path_folder_icons.'/'.$icon."'".');"> </div>'.
+									echo	'<td class="entry small">'.												
+												'<a name="icon-link" id="'.$UUID.'">'.
+													'<div class="entry-icon small container">'.
+														'<div class="entry-icon" style="background-image: url('."'".$path_folder_icons.'/'.$icon."'".');"> </div>'.
+														'<canvas width="300" height="300"></canvas>'.														
+													'</div>'.
 													'<div class="icon-name">'.$name.'</div>'.
-												'</a>'.										
+												'</a>'.												
 											'</td>';
 									
 									if(fmod($i, 5) == 4)
