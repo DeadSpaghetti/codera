f0c8440fb8a455847317364b7d2165c726ffba6e
spaghettic0der <max.wittig95@gmail.com>
2016-03-14 00:10:38 +0100

    preparations for setting of the sortOrder

--- php/helper/setGeneralSettingsToJSON.php
+++ php/helper/setGeneralSettingsToJSON.php
@@ -3,6 +3,7 @@
 $developerName = $_POST['developerName'];
 $colorScheme = $_POST['colorScheme'];
 $gridSize = $_POST['gridSize'];
+$sortOrder = $_POST['sortOrder'];
 //codera version is read only --> is only set by updater
 
 $generalSettingsFilename = "../../config/generalSettings.json";
@@ -11,7 +12,8 @@ $file = fopen($generalSettingsFilename,"w");
 $array = array(
   "developerName" => $developerName,
     "colorScheme" => $colorScheme,
-    "gridSize" => $gridSize
+    "gridSize" => $gridSize,
+    "sortOrder" => $sortOrder
 );
 
 $jsonString = json_encode($array);
