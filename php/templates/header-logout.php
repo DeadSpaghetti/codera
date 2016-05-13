882b38d1b98b8972f5d5651f613af6302d8bec3f
deadlocker8 <deadlocker@gmx.de>
2016-05-13 15:48:43 +0200

    Fixed #208

--- php/templates/header-logout.php
+++ php/templates/header-logout.php
@@ -38,8 +38,8 @@ ACCOUNT;
 	<div style="clear: both;"></div>
 
 </div>
-<div id="name" style="background-color: <?php echo $colorScheme;?>;">
-	<div class="name-text" id="name-text-<?php if(strlen($developerName) <= 10){echo "short";} else if(strlen($developerName) <= 20) {echo "middle";} else{ echo "long";}?>">
-		<a href="<?php echo $path_index;?>"><?php if(isset($developerName)) echo $developerName;?></a>
+<div id="name" style="background-color: <?php if(isset($colorScheme)) echo $colorScheme;?>;">
+	<div class="name-text" id="name-text-<?php if(strlen($developerName) <= 10){echo "short";} else if(strlen($developerName) <= 20) {echo "middle";} else{ echo "long";} ?>">
+		<a href="<?php echo $path_index;?>"  <?php if(strtolower($colorScheme) == "#fff176"){ echo 'style="color: #212121;"';}?>><?php if(isset($developerName)) echo $developerName; ?></a>
 	</div>			
 </div>
