a9bfb58da5008fe66df67cf6cd4adc42dd36bf21
spaghettic0der <max.wittig95@gmail.com>
2016-05-20 11:43:11 +0200

    changed footer url

--- php/templates/footer.php
+++ php/templates/footer.php
@@ -13,7 +13,13 @@ include($path_helper_getGeneralSettings);
 		<tr>
 			<td> <a href="<?php if(isset($path_about)) echo $path_about;?>">About</a></td>
 			<td>&copy; <?php if(isset($developerName)) echo $developerName;?> <?php echo date("Y");?></td>
-			<td> <div id="footer-right"><a href="https://github.com/spaghettic0der/codera"> <img id="icon-github" src="<?php if(isset($path_footer_github_icon)) echo $path_footer_github_icon;?>" alt=""> <span id="footer-right-text">codera on GitHub</span></a></div><div style="clear: both;"></div> </td>
+			<td>
+				<div id="footer-right"><a href="https://github.com/DeadSpaghetti/codera"> <img id="icon-github"
+																							   src="<?php if (isset($path_footer_github_icon)) echo $path_footer_github_icon; ?>"
+																							   alt=""> <span
+							id="footer-right-text">codera on GitHub</span></a></div>
+				<div style="clear: both;"></div>
+			</td>
 		</tr>
 	</table>		
 </div>
