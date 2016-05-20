baa98f663dfe9f9951ffcc8df506cfa74db01be6
spaghettic0der <max.wittig95@gmail.com>
2016-05-20 11:40:52 +0200

    changed update url

--- php/helper/updateCodera.php
+++ php/helper/updateCodera.php
@@ -9,7 +9,7 @@ if ($_SESSION['loggedIn'] != "admin")
     header('Location: ../login.php');
     exit;
 }
-$url = "http://max-spaghettic0der.rhcloud.com/codera-master.zip";
+$url = "https://github.com/DeadSpaghetti/codera/archive/1.0.0.zip";
 $copyLocation = "../../../";
 $downloadLocation = "../../../codera-master.zip";
 downloadZip($url, $downloadLocation);
