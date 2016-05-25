2e8654caa3322cbad9336ab089e3256dd3116108
spaghettic0der <max.wittig95@gmail.com>
2016-05-25 10:46:27 +0200

    fixed #225

---
+++ js/about.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var a_tags = document.getElementsByTagName("a");
+    for (var i = 0; i < a_tags.length; i++) {
+        if (a_tags[i].id == "" && a_tags[i].className == "" && a_tags[i].parentNode.classList.contains("about-right"))
+            a_tags[i].className = "link-visible";
+    }
+
+});
