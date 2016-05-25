d37e6a5e1b999c7bbe758a47774cf81e84d2fc9b
spaghettic0der <max.wittig95@gmail.com>
2016-05-25 10:50:05 +0200

    fix code formatting

--- js/about.js
+++ js/about.js
@@ -1,6 +1,8 @@
-$(document).ready(function () {
+$(document).ready(function ()
+{
     var a_tags = document.getElementsByTagName("a");
-    for (var i = 0; i < a_tags.length; i++) {
+    for (var i = 0; i < a_tags.length; i++)
+    {
         if (a_tags[i].id == "" && a_tags[i].className == "" && a_tags[i].parentNode.classList.contains("about-right"))
             a_tags[i].className = "link-visible";
     }
